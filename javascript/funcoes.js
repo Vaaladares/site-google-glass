a11fb4aa38c8aeb69aa8825d39cf8a4d7782db66
Vinicius Valadares Chinelatto <103156091+Vaaladares@users.noreply.github.com>
2022-12-30 20:22:32 -0300

    Atualizando

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -5,3 +5,10 @@ function menubar() {
         navbar.style.display = 'block'
     }
 }
+
+function enviar() {
+    let quant = document.getElementById('quant')
+    if (quant.value > 10) {
+        window.alert('Erro! Quantidade de óculos limitado a 10 unidades.')
+    }
+}
